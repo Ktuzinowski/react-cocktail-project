e9f0abfaa5cca10063ef8fac86179a45bac8b408
Ktuzinowski <67128477+Ktuzinowski@users.noreply.github.com>
2021-07-08 19:43:49 -0400

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, dolorem
+        voluptates? Rerum, ipsam? Cupiditate voluptates, laborum corporis
+        blanditiis adipisci suscipit repellat! Quasi, eveniet amet officiis
+        minima culpa placeat. Cumque, consequuntur.
+      </p>
+    </section>
+  );
+};
+
+export default About;
